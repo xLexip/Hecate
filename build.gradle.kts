0cd69199fe18b4b5d365b096d6e19e464da57bd9
xLexip <x@lexip.dev>
2024-10-27 20:52:41 +0100

    chore: Integrate SonarCloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("org.sonarqube") version "5.1.0.4882"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "xLexip_Hecate")
+        property("sonar.organization", "xlexip")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
