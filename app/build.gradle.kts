2dfd167b35b80b113446fe5e0afebb05fcdb95ed
xLexip <x@lexip.dev>
2026-08-26 14:52:53 +0200

    build: v2.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "dev.lexip.hecate"
         minSdk = 34
         targetSdk = 36
-        versionCode = 133
-        versionName = "2.4.0"
+        versionCode = 136
+        versionName = "2.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
